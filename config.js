5d4caed98cc480a94863b84ec95ffa1acc3e1a1a
maaahe <38563817+maaahe@users.noreply.github.com>
2026-07-01 15:34:26 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.STREAKS_CONFIG = {
+  supabaseUrl: "https://meidjiocjhwlfqrtoizu.supabase.co",
+  supabaseAnonKey: "sb_publishable_4nZsjW5dP_5NHx6QuProwQ_ubAMWucy",
+  stateId: "personal-streaks",
+};
